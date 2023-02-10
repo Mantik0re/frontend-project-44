62477050874ff5efa29226ac1783880876cd0821
Mantik0re <112542534+Mantik0re@users.noreply.github.com>
2023-02-10 18:28:48 +0300

    Delete brain-calc.js

--- bin/brain-calc.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-import welcomeMessage from '../src/cli.js';
-import playGame from '../src/index.js';
-import checkingNumber from '../src/games/calcgames.js';
-
-
-let username = welcomeMessage();
-playGame(username, checkingNumber);
