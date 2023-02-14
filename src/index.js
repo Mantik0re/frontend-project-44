6e46265c202dc9ef24493a8201a647fbc3abac9d
Elza Korennaya <98534263+Elzakor@users.noreply.github.com>
2023-02-14 18:03:06 +0300

    minor fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-
-import readlineSync from 'readline-sync';
-
-
 const playGame = (userName, checkingFunc) => {
 
   let correctAnswersCount = 0;
   while (correctAnswersCount < 3) {
     if (checkingFunc() === false) {
+      qqq
       console.log(`Let's try again, ${userName}!`);
       return;
     }
