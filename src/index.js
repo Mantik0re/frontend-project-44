12336d09e2d2de13d631e771d57dbb185009760c
Elza Korennaya <98534263+Elzakor@users.noreply.github.com>
2023-03-23 00:31:04 +0300

    change function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
+import readlineSync from 'readline-sync';
+// import welcomeMessage from '../src/cli.js';
+
+
+const maxCorrectAnswerCount = 3;
+
+
 const playGame = (userName, checkingFunc) => {
+    let correctAnswersCount = 0;
+    while (correctAnswersCount < maxCorrectAnswerCount) {
+        let gameData = checkingFunc();
+        let gameQuestion = gameData['question'];
+        let gameAnswer = gameData['answer'];
 
-  let correctAnswersCount = 0;
-  while (correctAnswersCount < 3) {
-    if (checkingFunc() === false) {
-      qqq
-      console.log(`Let's try again, ${userName}!`);
-      return;
+        console.log(`${gameQuestion}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+
+        if (userAnswer != gameAnswer) {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gameAnswer}'.`);
+            console.log(`Let's try again, ${userName}!`);
+            return;
+        }
+        console.log('Correct!');
+        correctAnswersCount += 1;
+
     }
-    correctAnswersCount += 1;
-
-  }
-  console.log(`Congratulations, ${userName}!`);
+    console.log(`Congratulations, ${userName}!`);
 };
 
 export default playGame;
