39acbb349a6fed4cc6c4daf4c13777285a05d2c0
Elza Korennaya <98534263+Elzakor@users.noreply.github.com>
2023-03-23 00:29:17 +0300

    rename function

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const welcomeMessage = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
-  return userName;
+const introduceUser = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}`);
+    return userName;
 };
 
-export default welcomeMessage;
-
+export default introduceUser;
