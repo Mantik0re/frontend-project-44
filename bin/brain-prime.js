9c793045da0b95bf0f9ba1b4bc48ed230902c6e1
Elza Korennaya <98534263+Elzakor@users.noreply.github.com>
2023-03-23 00:32:00 +0300

    correct minor issues

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-import welcomeMessage from '../src/cli.js';
+import introduceUser from '../src/cli.js';
 import playGame from '../src/index.js';
 import checkingNumber from '../src/games/primegame.js';
 
 
-let username = welcomeMessage();
-playGame(username, checkingNumber);
+let userName = introduceUser();
+
+playGame(userName, checkingNumber);
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-import welcomeMessage from '../src/cli.js';
+import introduceUser from '../src/cli.js';
 import playGame from '../src/index.js';
 import checkingNumber from '../src/games/progressiongames.js';
 
 
-let username = welcomeMessage();
-playGame(username, checkingNumber);
+let userName = introduceUser();
+
+playGame(userName, checkingNumber);
