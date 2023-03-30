815526db428a00735773df5b79735f5e84ee9677
Elza Korennaya <elzakv18@gmail.com>
2023-03-30 16:01:53 +0300

    fix check func

--- src/games/primegame.js
+++ src/games/primegame.js
@@ -2,17 +2,22 @@
 import randomNumber from '../utils.js';
 
 // eslint-disable-next-line consistent-return
-const IsNumberEven = (num) => {
-  if (num % 2 === 0) {
-    return 'yes';
+const IsNumberPrime = (num) => {
+  if (num <= 1) return 'no';
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return 'no';
+    }
   }
-  return 'no';
+  return 'yes'
 };
 
 const checkingNumber = () => {
   const checkingNum = randomNumber() || 1;
   const question = `Answer "yes" if given number is prime. Otherwise answer "no". \nQuestion: ${checkingNum}`;
-  const answer = IsNumberEven(checkingNum);
+  const answer = IsNumberPrime(checkingNum);
 
   return { question, answer };
 };
