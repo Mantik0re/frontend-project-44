729940505c7f27becf987b71955550e935acb377
Elza Korennaya <elzakv18@gmail.com>
2023-03-30 14:09:18 +0300

    fix: fix code cosmetic issue

--- src/games/evengame.js
+++ src/games/evengame.js
@@ -1,19 +1,19 @@
+// eslint-disable-next-line import/extensions
 import randomNumber from '../utils.js';
 
 const IsNumberEven = (num) => {
-    if (num % 2 === 0) {
-        return 'yes';
-    } else {
-        return 'no';
-    }
+  if (num % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
 };
 
 const checkingNumber = () => {
-    const checkingNum = randomNumber() || 1;
-    const question = `Answer "yes" if the number is even, otherwise answer "no". \nQuestion: ${checkingNum}`;
-    const answer = IsNumberEven(checkingNum);
+  const checkingNum = randomNumber() || 1;
+  const question = `Answer "yes" if the number is even, otherwise answer "no". \nQuestion: ${checkingNum}`;
+  const answer = IsNumberEven(checkingNum);
 
-    return { 'question': question, 'answer': answer };
+  return { question, answer };
 };
 
 export default checkingNumber;
